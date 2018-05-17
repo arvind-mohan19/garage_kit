b151717127784bd982c9138805abc2a0021a33eb
ArvindM <arvind.mohan@freshworks.com>
2018-05-17 18:01:26 +0530

    removing unnecessary files

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,4 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
-
+  	get 'sessions/new'
 	resources :users, :spaces, :appointments, :ratings, format: "json"
-
-
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-	get "/verify" => 'verify#new', as: 'new_verify'
-	put '/verify' => 'verify#update', as: 'verify'
-	post '/verify' => 'verify#create', as: 'resend_verify'
 end
