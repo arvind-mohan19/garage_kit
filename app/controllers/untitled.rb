353519d075f9cd4bad103c11bedad95c9a3f5886
ArvindM <arvind.mohan@freshworks.com>
2018-05-17 18:00:00 +0530

    useless

--- app/controllers/untitled.rb
+++
@@ -1,1 +0,0 @@
-$sql = "SELECT *, ( 6371 * acos( cos( radians(" . #{params[:latitude]} . ") ) * cos( radians( lat ) ) * cos( radians( longitude ) - radians(" . #{params[:longitude]} . ") ) + sin( radians(" . #{params[:latitude]} . ") ) * sin( radians( latitude ) ) ) ) AS distance FROM spaces HAVING distance < 5";
